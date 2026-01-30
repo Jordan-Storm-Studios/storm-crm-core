f38e3048b0c21932307ac348108ee0e59185c66e
Jordan-Storm-Studios <Jordan@stormstudios.co>
2026-01-30 08:58:16 +0000

    Update contact.py

--- app/schemas/contact.py
+++ app/schemas/contact.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+
+class ContactCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+
+class ContactRead(ContactCreate):
+    id: UUID
+
+    class Config:
+        from_attributes = True
 

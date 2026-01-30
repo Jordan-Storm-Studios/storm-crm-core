5ab6905de14dddb55d8f98557af745ef7a06407b
Jordan-Storm-Studios <Jordan@stormstudios.co>
2026-01-30 09:04:57 +0000

    Update contact.py

--- app/schemas/contact.py
+++ app/schemas/contact.py
@@ -2,12 +2,14 @@ from pydantic import BaseModel, EmailStr
 from uuid import UUID
 
 class ContactCreate(BaseModel):
-    first_name: str
-    last_name: str
+    first_name: str | None = None
+    last_name: str | None = None
     email: EmailStr
 
 class ContactRead(ContactCreate):
     id: UUID
+    status: str
+    suppressed: bool
 
     class Config:
         from_attributes = True

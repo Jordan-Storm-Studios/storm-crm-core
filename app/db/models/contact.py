5a435b634d37260117c8df09f0196f0d3f20b52d
Jordan-Storm-Studios <Jordan@stormstudios.co>
2026-01-30 08:57:13 +0000

    Update contact.py

--- app/db/models/contact.py
+++ app/db/models/contact.py
@@ -1,1 +1,13 @@
+from sqlalchemy import Column, String, Boolean, UUID
+from app.db.base import Base
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(UUID, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True, index=True)
+    status = Column(String)
+    suppressed = Column(Boolean)
 
